a65accaab2f86363a5907d42ec30125f6fbf0e30
LeeJongbokZ <s2feeling@naver.com>
2023-05-02 17:53:12 +0900

    Notification 변경 사항 추가

--- src/main/java/com/example/interviewPrep/quiz/notification/controller/NotificationController.java
+++ src/main/java/com/example/interviewPrep/quiz/notification/controller/NotificationController.java
@@ -1,4 +1,49 @@
-package com.example.interviewPrep.quiz.notification;
+package com.example.interviewPrep.quiz.notification.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.interviewPrep.quiz.notification.service.NotificationService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@RestController
 public class NotificationController {
+
+    private final NotificationService notificationService;
+
+    public NotificationController(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+
+    /**
+     * @title 로그인 한 유저 sse 연결
+     */
+    @GetMapping(value = "/subscribe", produces = "text/event-stream")
+    public SseEmitter subscribe(@RequestParam(value = "lastEventId", required = false, defaultValue = "") String lastEventId) {
+        return notificationService.subscribe(lastEventId);
+    }
+
+
+    /**
+     * @title 로그인 한 유저의 모든 알림 조회
+     */
+    /*
+    @GetMapping("/notifications")
+    public ResponseEntity<NotificationsResponse> notifications(@Login LoginMember loginMember) {
+        return ResponseEntity.ok().body(notificationService.findAllById(loginMember));
+    }
+
+    /**
+     * @title 알림 읽음 상태 변경
+     */
+    /*
+    @PatchMapping("/notifications/{id}")
+    public ResponseEntity<Void> readNotification(@PathVariable Long id) {
+        notificationService.readNotification(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+    */
 }
