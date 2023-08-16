82fe7a31456551b103dd05ff0a41c139f6e49db5
LeeJaeyun7 <jaeyun07077@gmail.com>
2023-08-16 12:59:49 +0900

    Mock Interview 기능을 위한 Entity 클래스 추가

---
+++ src/main/java/com/example/interviewPrep/quiz/company/domain/Company.java
@@ -0,0 +1,41 @@
+package com.example.interviewPrep.quiz.company.domain;
+
+
+import com.example.interviewPrep.quiz.domain.BaseTimeEntity;
+import com.example.interviewPrep.quiz.question.domain.Question;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+public class Company extends BaseTimeEntity {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "COMPANY_ID")
+    private Long id;
+    private String name;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "QUESTION_ID")
+    Question question;
+    public Company(Long id, String name){
+
+        Objects.requireNonNull(id, "id가 null입니다.");
+        Objects.requireNonNull(name, "name이 null입니다.");
+
+        this.id = id;
+        this.name = name;
+    }
+
+
+}
---
+++ src/main/java/com/example/interviewPrep/quiz/interview/domain/Interview.java
@@ -0,0 +1,30 @@
+package com.example.interviewPrep.quiz.interview.domain;
+
+
+import com.example.interviewPrep.quiz.domain.BaseTimeEntity;
+import com.example.interviewPrep.quiz.member.domain.Member;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+public class Interview extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "INTERVIEW_ID")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+
+}
