ef8a812f3cb35a5ed4017285112c56fa8d9d57f1
LeeJaeyun7 <jaeyun07077@gmail.com>
2023-08-21 22:43:37 +0900

    MonthDuration months 필드 final 추가

--- src/main/java/com/example/interviewPrep/quiz/utils/MonthDuration.java
+++ src/main/java/com/example/interviewPrep/quiz/utils/MonthDuration.java
@@ -3,7 +3,7 @@ package com.example.interviewPrep.quiz.utils;
 public enum MonthDuration {
     ONE(1), THREE(3), SIX(6), TWELVE(12);
 
-    private int months;
+    private final int months;
 
     private MonthDuration(int months) {
         this.months = months;
