873238084baa5130250582f568d4498167e64a1c
LeeJaeyun7 <jaeyun07077@gmail.com>
2023-08-28 09:49:49 +0900

    Question 패키지 버그 수정, Answer 엔티티 createAnswerEntity 메소드 추가

--- src/main/java/com/example/interviewPrep/quiz/questionCompany/domain/QuestionCompany.java
+++ src/main/java/com/example/interviewPrep/quiz/questionCompany/domain/QuestionCompany.java
@@ -2,6 +2,7 @@ package com.example.interviewPrep.quiz.questionCompany.domain;
 
 
 import com.example.interviewPrep.quiz.company.domain.Company;
+import com.example.interviewPrep.quiz.domain.BaseTimeEntity;
 import com.example.interviewPrep.quiz.question.domain.Question;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,7 +13,6 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-
 import java.util.Objects;
 
 import static javax.persistence.FetchType.LAZY;
@@ -20,22 +20,22 @@ import static javax.persistence.FetchType.LAZY;
 @Entity
 @Getter
 @Setter
-@Builder
 @NoArgsConstructor
-public class QuestionCompany {
+public class QuestionCompany extends BaseTimeEntity {
 
     @Id
+    private Long id;
+
     @ManyToOne(fetch = LAZY)
-    @JoinColumn(name="QUESTION_ID")
+    @JoinColumn(name = "QUESTION_ID")
     private Question question;
 
-    @Id
     @ManyToOne(fetch = LAZY)
-    @JoinColumn(name="COMPANY_ID")
+    @JoinColumn(name = "COMPANY_ID")
     private Company company;
 
-
-    public QuestionCompany(Question question, Company company){
+    @Builder
+    public QuestionCompany(Question question, Company company) {
 
         Objects.requireNonNull(question, "question이 null입니다.");
         Objects.requireNonNull(company, "company가 null입니다.");
