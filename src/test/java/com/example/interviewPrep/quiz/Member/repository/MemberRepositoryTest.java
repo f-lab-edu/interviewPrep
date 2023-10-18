c12cd2fab0bb22b1626da73ec9967113a2db499f
LeeJongbokZ <s2feeling@naver.com>
2023-10-18 23:46:31 +0900

    remove unused imports

--- src/test/java/com/example/interviewPrep/quiz/Member/repository/MemberRepositoryTest.java
+++ src/test/java/com/example/interviewPrep/quiz/Member/repository/MemberRepositoryTest.java
@@ -6,12 +6,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
 
