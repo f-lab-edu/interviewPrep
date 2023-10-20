c07561e1124923ac65d1037dd5ff3f5a0fa69b46
LeeJongbokZ <s2feeling@naver.com>
2023-10-20 17:00:49 +0900

    Answer 패키지 테스트 업데이트

--- src/test/java/com/example/interviewPrep/quiz/Answer/domain/AnswerTest.java
+++ src/test/java/com/example/interviewPrep/quiz/Answer/domain/AnswerTest.java
@@ -1,38 +1,42 @@
 package com.example.interviewPrep.quiz.Answer.domain;
 
 import com.example.interviewPrep.quiz.answer.domain.Answer;
+import com.example.interviewPrep.quiz.member.domain.Member;
+import com.example.interviewPrep.quiz.question.domain.Question;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-
+@ExtendWith(MockitoExtension.class)
 public class AnswerTest {
+
+    @Mock
+    Question question;
+
+    @Mock
+    Member member;
 
 
     @Test
-    void creationWithId(){
+    void createAnswer(){
         Answer answer = Answer.builder()
+                        .question(question)
+                        .member(member)
                         .content("답안입니다.")
                         .build();
 
-        assertThat(answer.getId()).isEqualTo(1L);
         assertThat(answer.getContent()).isEqualTo("답안입니다.");
-    }
-
-
-    @Test
-    void creationWithoutId(){
-        Answer answer = Answer.builder()
-                        .content("답안입니다2.")
-                        .build();
-
-        assertThat(answer.getContent()).isEqualTo("답안입니다2.");
     }
 
 
     @Test
     void change(){
         Answer answer = Answer.builder()
+                        .question(question)
+                        .member(member)
                         .content("답안입니다.")
                         .build();
 
