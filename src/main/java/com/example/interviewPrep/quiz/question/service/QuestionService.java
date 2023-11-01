d7257a7e11abe6a44e33e5150d6591240f471783
LeeJongbokZ <s2feeling@naver.com>
2023-11-01 21:01:39 +0900

    remove unused imports

--- src/main/java/com/example/interviewPrep/quiz/question/service/QuestionService.java
+++ src/main/java/com/example/interviewPrep/quiz/question/service/QuestionService.java
@@ -4,7 +4,6 @@ import com.example.interviewPrep.quiz.answer.repository.AnswerRepository;
 import com.example.interviewPrep.quiz.company.domain.Company;
 import com.example.interviewPrep.quiz.company.repository.CompanyRepository;
 import com.example.interviewPrep.quiz.exception.advice.CommonException;
-import com.example.interviewPrep.quiz.exception.advice.ErrorCode;
 import com.example.interviewPrep.quiz.jwt.service.JwtService;
 import com.example.interviewPrep.quiz.question.domain.Question;
 import com.example.interviewPrep.quiz.question.dto.FilterDTO;
